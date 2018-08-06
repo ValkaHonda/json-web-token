283057becaa9358d2fe7cb13818fa0f33a51f058
Lenny <valentin805@gmail.com>
2018-08-06 15:19:03 +0300

    Add project scelet and import PostgreSQL

---
+++ src/main/java/com/quickjwt/repositories/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.quickjwt.repositories;
+
+import com.quickjwt.entities.Authority;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorityRepository extends CrudRepository<Authority, Long> {
+
+    Authority findOneByAuthority(String authority);
+}
---
+++ src/main/java/com/quickjwt/services/UserService.java
@@ -0,0 +1,46 @@
+package com.quickjwt.services;
+
+import com.quickjwt.entities.Authority;
+import com.quickjwt.entities.User;
+import com.quickjwt.exceptions.UserNotFoundException;
+import com.quickjwt.models.RegistrationModel;
+import com.quickjwt.repositories.AuthorityRepository;
+import com.quickjwt.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findOneByUsername(username);
+        if(user == null){
+            throw new UserNotFoundException("User not found");
+        }
+
+        return user;
+    }
+
+    public void register(RegistrationModel registrationModel){
+        User user = new User();
+        user.setUsername(registrationModel.getUsername());
+        user.setPassword(passwordEncoder.encode(registrationModel.getPassword()));
+        Authority authority = this.authorityRepository.findOneByAuthority("ROLE_USER");
+        user.getAuthorities().add(authority);
+        this.userRepository.save(user);
+    }
+}
