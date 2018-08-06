5a28c965b6823cac279fd6cff33153db568a8f4f
Lenny <valentin805@gmail.com>
2018-08-06 16:56:34 +0300

    Add Json Web Token configurer

--- src/main/java/com/quickjwt/configuration/jwt/JWTConfigurer.java
+++ src/main/java/com/quickjwt/configuration/jwt/JWTConfigurer.java
@@ -5,10 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-/**
- * Created by Lenny on 8/6/2018.
- */
 public class JWTConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
     public static final String AUTHORIZATION_HEADER = "Authorization";
 
     private TokenProvider tokenProvider;
@@ -22,5 +20,5 @@ public class JWTConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilt
         JWTFilter customFilter = new JWTFilter(tokenProvider);
         http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
     }
-
 }
+
--- src/main/java/com/quickjwt/configuration/jwt/JWTFilter.java
+++ src/main/java/com/quickjwt/configuration/jwt/JWTFilter.java
@@ -1,6 +1,8 @@
 package com.quickjwt.configuration.jwt;
 
 import io.jsonwebtoken.ExpiredJwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -11,13 +13,12 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by Lenny on 8/6/2018.
- */
 public class JWTFilter extends GenericFilterBean {
 
+    private final Logger log = LoggerFactory.getLogger(JWTFilter.class);
     private TokenProvider tokenProvider;
 
     public JWTFilter(TokenProvider tokenProvider) {
@@ -36,9 +37,14 @@ public class JWTFilter extends GenericFilterBean {
             }
             filterChain.doFilter(servletRequest, servletResponse);
         } catch (ExpiredJwtException eje) {
+            log.info("Security exception for user {} - {}",
+                    eje.getClaims().getSubject(), eje.getMessage());
 
+            log.trace("Security exception trace: {}", eje);
+            ((HttpServletResponse) servletResponse).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }
     }
+
     private String resolveToken(HttpServletRequest request){
         String bearerToken = request.getHeader(JWTConfigurer.AUTHORIZATION_HEADER);
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
@@ -46,5 +52,4 @@ public class JWTFilter extends GenericFilterBean {
         }
         return null;
     }
-
 }
